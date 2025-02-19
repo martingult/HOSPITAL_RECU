ea9aa990e09cfd1bc631bc27bf707786029d6c9d
martingult <martinat@msmk.university>
2025-02-19 21:07:09 +0100

    Agregar archivos de proyecto.

---
+++ HOSPITAL_RECU.cpp
@@ -0,0 +1,12 @@
+﻿// HOSPITAL_RECU.cpp: define el punto de entrada de la aplicación.
+//
+
+#include "HOSPITAL_RECU.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
